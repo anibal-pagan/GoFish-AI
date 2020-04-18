b9c9582f37c09fc65fe90e86d2211b53c696ddbe
Estefania Torres <estefania.torres1@upr.edu>
2020-04-17 20:58:38 -0400

    added player

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
     public static void main(String[] args){
         Deck deck = new Deck();
+        Player p1 = new Player("p1");
+
+        deck.giveInitialCards(p1);
+        System.out.println(deck.getDeckSize());
     }
 
 }
