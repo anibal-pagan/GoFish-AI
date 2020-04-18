7a38fa8408c4cc78d74d3e9cbfc285b2354b27af
Anibal Pagan <anibal.pagan@upr.edu>
2020-04-17 23:15:27 -0400

    Added most of game functionality.

--- src/Player.java
+++ src/Player.java
@@ -17,4 +17,27 @@ public class Player {
         myCards.add(c);
     }
 
+    public void displayCardsInHand() {
+        for(int i=0; i<myCards.size(); i++) {
+            int num = i+1;
+            System.out.println(num + ") "+ myCards.toString());
+        }
+    }
+
+    //Returns number of cards given
+    public int giveCards(int cardNum, Player p) {
+        int cardsGiven = 0;
+        ArrayList<Card> cardsToRemove = new ArrayList<>();
+        for(Card c : myCards) {
+            if (c.getNumber() == cardNum) {
+                cardsToRemove.add(c);
+                p.giveCard(c);
+            }
+        }
+        for(Card c : cardsToRemove) {
+            myCards.remove(c);
+        }
+        return cardsGiven;
+    }
+
 }
